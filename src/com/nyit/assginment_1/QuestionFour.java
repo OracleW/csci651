d4cd8d9bf9a9b88a729854ac2399c21462e95fad
Wen Han <wen@WendeMacBook-Air.local>
2022-10-17 11:30:43 -0700

    Initial commit

---
+++ src/com/nyit/assginment_1/QuestionFour.java
@@ -0,0 +1,34 @@
+public class QuestionFour {
+    public static void main(String[] args) {
+        QuestionFour qf = new QuestionFour();
+
+//        String result = qf.divideUtil(0,);
+
+    }
+
+
+
+    private String mergeUtil(String left, String right) {
+        String common = "";
+        for (int i = 0, j = 0; i < left.length() && j < right.length(); i++, j++) {
+            if (left.charAt(i) != right.charAt(j))
+                break;
+            common += left.charAt(i);
+        }
+        return common;
+    }
+
+    private String divideUtil(int begin, int end, String[] team) {
+        if (begin == end)
+            return team[begin];
+        if (end > begin) {
+            int middle = (begin + end) / 2;
+            String left = divideUtil(begin, middle, team);
+            String right = divideUtil(middle + 1, end, team);
+            return mergeUtil(left, right);
+        }
+        return null;
+    }
+
+
+}
